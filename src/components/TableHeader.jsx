960cdc11d22a05c51dccae7a4a352c3900a0e772
jstgrowup <deysubham999@gmail.com>
2024-01-10 21:28:33 +0530

    fix

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TableHeader = ({ content }) => {
   return (
-    <div className="w-[161px] h-[38px] py-[16px] px-[8px] border  flex items-center justify-center bg-gray-100 text-gray-600 text-[12px] font-bold uppercase">
+    <div className="w-[161px] h-[38px] py-[16px] px-[8px] border flex items-center justify-center bg-gray-100 text-gray-600 text-[14px] font-bold">
       <p>{content}</p>
     </div>
   );
